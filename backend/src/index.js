08de440919f8128ae28d309a98897ec5389f7ae9
Handradus <humberto.andrades2201@alumnos.ubiobio.cl>
2025-06-01 23:08:36 -0400

    MOD variables env

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,14 +3,13 @@ const { AppDataSource } = require('./data-source');
 require('dotenv').config();
 
 const cartaRoutes = require('./routes/cartaRoutes');
-const importarRoutes = require('./routes/importarRoutes');
 const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 3000;
 
 AppDataSource.initialize()
@@ -19,11 +18,11 @@ AppDataSource.initialize()
 
     // Rutas
     app.use('/api/cartas', cartaRoutes);
-    app.use('/api/importar', importarRoutes);
+    
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`);
-    });
+    app.listen(PORT, HOST, () => {
+  console.log(`🚀 Servidor escuchando en http://${HOST}:${PORT}`);
+  });
   })
   .catch((error) => {
     console.error('❌ Error al conectar con la base de datos:', error);
