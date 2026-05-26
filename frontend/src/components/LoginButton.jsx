e04c8b3db6937d95db70d0ed7cfd80c80f5eaae4
Handradus <Humberto.andrades2201@alumnos.ubiobio.cl>
2026-05-26 00:16:30 -0400

    Fix pop up oauth

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -37,7 +37,7 @@ const LoginButton = () => {
         <GoogleLogin
             onSuccess={onSuccess}
             onError={onError}
-            useOneTap
+            ux_mode="popup"
         />
     );
 };
